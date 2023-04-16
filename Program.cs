4a850dd088706bc42d64565be2a56234e5d23c1a
YGumarov <yerassylgumarov@gmail.com>
2023-04-16 12:09:49 +0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment2;
+
+public interface IMyList<T>
+{
+    int Size();
+    bool Contains(Object o);
+    void Add(T item);
+    void Add(T item, int index);
+    bool Remove(T item);
+    T Remove(int index);
+    void Clear();
+    T Get(int index);
+    int Indexof(Object o);
+    int Lastindexof(Object o);
+    void Sort();
+}
+
+public class MyArrayList<T> : IMyList<T>
+{
+    private T[] elements;
+    private int size;
+
+    public MyArrayList()
+    {
+        elements = new T[10];
+        size = 0;
+    }
+    
+}
